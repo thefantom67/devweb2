5d3afdc7aa61ff9924bb4c723e53d8d5b0fae0f3
Bruno Malvy <Bruno@MacBook-Pro-de-Bruno.local>
2019-10-30 00:12:28 +0100

    Fix(): Increment produit/ panier not working

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Store } from '@ngxs/store';
-import {Observable  } from 'rxjs';
-import {Hotel} from '../models/hotel';
+import { Observable } from 'rxjs';
+import { Hotel } from '../models/hotel';
+import {PanierState} from '../../../shared/stats/panier-state';
+import { DelHotel } from '../../../shared/actions/article-delaction';
 
 @Component({
   selector: 'app-panier',
@@ -10,8 +12,15 @@ import {Hotel} from '../models/hotel';
 })
 export class PanierComponent implements OnInit {
 
-  constructor() { }
+  panier: Observable<Hotel>; 
+  
+  constructor(private store: Store) {this.panier = this.store.select(state => state.panier.panier);}
 
+  onDelClick () {
+    this.DelHotel();
+  }
+  DelHotel() { this.store.dispatch(new DelHotel()); }
+  
   ngOnInit() {
   }
 
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Store } from '@ngxs/store';
 import { SwipeService } from '../services/swipe.service';
 import { Hotel } from '../models/hotel';
+import { AddHotel } from 'shared/actions/article-action';
 
 @Component({
   selector: 'app-produits',
@@ -10,10 +12,23 @@ import { Hotel } from '../models/hotel';
 })
 export class ProduitsComponent implements OnInit {
 
-  constructor(private SwipeService : SwipeService) { }
+  nom : string;
+  prix: string;
+  img: string;
+
+  constructor(private SwipeService : SwipeService, private store: Store) {}
   Hotels : Observable<Hotel[]>
+  panier: Observable<Hotel>
+
+  onClick () {
+    this.addHotel(this.nom, this.prix, this.img);
+  }
+
+  addHotel(nom, prix, img) { this.store.dispatch(new AddHotel({nom,prix,img})); }
+
   ngOnInit() {
     this.Hotels = this.SwipeService.getHotels ();
+    this.panier = this.store.select(state => state.panier.panier);
   }
 
 }
